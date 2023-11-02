583cc1b848994db34c0c18405b8673d97b8ae14b
Lenovo <Lenovo@DESKTOP-1KQ9P4G>
2023-11-02 10:53:01 +0100

    API - add Blog Post functionality (HTTP Post)

---
+++ CodePulse.API/Controllers/BlogPostController.cs
@@ -0,0 +1,66 @@
+﻿using CodePulse.API.Models.Domain;
+using CodePulse.API.Models.DTO;
+using CodePulse.API.Repositories.Interface;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CodePulse.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BlogPostController : ControllerBase
+    {
+        private readonly IBlogPostRepository blogPostRepository;
+
+        public BlogPostController(IBlogPostRepository blogPostRepository)
+        {
+            this.blogPostRepository = blogPostRepository;
+        }
+
+
+        // POST: {apibaseurl}/api/blogposts
+        [HttpPost]
+        //[Authorize(Roles = "Writer")]
+        public async Task<IActionResult> CreateBlogPost([FromBody] CreateBlogPostRequestDto request)
+        {
+            // Convert DTO to DOmain
+            var blogPost = new BlogPost
+            {
+                Author = request.Author,
+                Content = request.Content,
+                FeaturedImageUrl = request.FeaturedImageUrl,
+                IsVisible = request.IsVisible,
+                PublishedDate = request.PublishedDate,
+                ShortDescription = request.ShortDescription,
+                Title = request.Title,
+                UrlHandle = request.UrlHandle,
+                //Categories = new List<Category>()
+            };
+
+            blogPost = await blogPostRepository.CreateAsync(blogPost);
+
+            // Convert Domain Model back to DTO
+            var response = new BlogPostDto
+            {
+                Id = blogPost.Id,
+                Author = blogPost.Author,
+                Content = blogPost.Content,
+                FeaturedImageUrl = blogPost.FeaturedImageUrl,
+                IsVisible = blogPost.IsVisible,
+                PublishedDate = blogPost.PublishedDate,
+                ShortDescription = blogPost.ShortDescription,
+                Title = blogPost.Title,
+                UrlHandle = blogPost.UrlHandle,
+                //Categories = blogPost.Categories.Select(x => new CategoryDto
+                //{
+                //    Id = x.Id,
+                //    Name = x.Name,
+                //    UrlHandle = x.UrlHandle
+                //}).ToList()
+            };
+
+            return Ok(response);
+        }
+    }
+}
