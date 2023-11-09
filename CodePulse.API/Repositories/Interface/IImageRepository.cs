33f6409ee6883da402ee0678c2e9da9d11a10e29
Lenovo <hassanyousef.de@gmail.com>
2023-11-10 00:07:42 +0100

    API - get all images

--- CodePulse.API/Repositories/Interface/IImageRepository.cs
+++ CodePulse.API/Repositories/Interface/IImageRepository.cs
@@ -5,6 +5,8 @@ namespace CodePulse.API.Repositories.Interface
     public interface IImageRepository
     {
         Task<BlogImage> Upload(IFormFile file, BlogImage blogImage);
+        Task<IEnumerable<BlogImage>> GetAll();
+
 
     }
 }
